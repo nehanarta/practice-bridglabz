c1a655c3ed7c1d06accbcdceafd4e500a62817b7
nehanarta <127373587+nehanarta@users.noreply.github.com>
2023-03-31 14:38:50 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,15 @@
+package Practice.Example;
+
+public class Array {
+    public static void main(String[]args){
+        int[]A=new int[10];
+        A[0]=1;
+        A[1]=3;
+        A[2]=4;
+        A[3]=7;
+        A[8]=8;
+        for(int i=0;i<A.length;i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
